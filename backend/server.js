9c3fa29b8d9a29c2577388036521e5544ca7c400
MrMothi <mailtime2@gmail.com>
2024-11-17 17:19:56 -0500

    got the backend functionality working with frontend, also got a single connection working that closes on termination

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,69 @@ const app = express();   //creating an express application
 require('dotenv').config(); //importing the env library
 const PORT = process.env.BACKEND_PORT;  //setting the server port to the environment variable
 
+//MIDDLEWARE
 app.use(cors()); // enabling cors
 app.use(express.json()); // getting express json reader functionality
+
+let oracleDBconnection;
+//injecting the oracle database connection variable into the req object of the nodejs server
+//this allows not having a global variable for the oracledb connection, and now other route handlers can just access this 
+//by calling req.dbConnection
+app.use((req, res, next) => { 
+    req.oracleConnection = oracleDBconnection; 
+    next(); 
+});
+
+
+
+//not implementing the process pool strategy for database connections since there will only be 1 server instance running for a single frontend
+async function initOracleConnection() { 
+     
+    try { 
+        oracleDBconnection = await oracledb.getConnection({ 
+            user: process.env.DB_USER, 
+            password: process.env.DB_PASS, 
+            connectString: process.env.DB_CONNECT_STRING
+        }); console.log('Successfully connected to Oracle Database');
+        // app.use((req, res, next) => { 
+        //     req.oracleConnection = oracleDBconnection; 
+        //     next(); 
+        // });
+        
+        app.locals.oracleConnection = oracleDBconnection
+
+        // Execute a simple query 
+        // const result = await req.oracleConnection.execute('SELECT * FROM customer'); 
+        // console.log(result.rows); 
+    } 
+    catch (error)     //for any errors in conecting to the DB
+    { 
+        console.error(error);
+        process.exit(1); 
+    } 
+}
+
+
+//function for closing the oracleDB connection
+async function closeOracleConnection() {
+    // if()
+    try {
+        console.log("Attempting to close oracle DB connection")
+        await app.locals.oracleConnection.close();
+        console.log("Closed oracle DB connection");
+        process.exit(0);
+    }
+    catch (error) {
+        console.error('Cannot close oracle DB connection', error);
+        process.exit(1);
+    }
+}
+
+
+
+//listening for any force quit cntl-c for the server, if there is any, then closes database conncetion
+process.once('SIGTERM', closeOracleConnection).once('SIGINT', closeOracleConnection);
+
 
 //default server backend page
 app.get('/', (req, res) => {
@@ -14,16 +75,29 @@ app.get('/', (req, res) => {
 })
 
 
-app.get('/get/product', (req, res) => { 
-    res.json({ message: 'Hello from the backend!' }); 
+app.get('/get/product', async (req, res) => { 
+    try{
+        console.log(req.oracleConnection)
+        const result = await req.oracleConnection.execute('SELECT * FROM customer'); 
+        // const result = await app.locals.oracleConnection.execute('SELECT * FROM customer');    //if using app locals variable
+        console.log("RESULT ROWS",result.rows); // can comment out later
+        const resultRows = result.rows;   //getting the rows of the result, since other stuff is uneccesary
+        res.json(resultRows);    //sending back the results in json format, using the res object json function (from express middleware)
+    } 
+    catch (error) {
+        res.status(500).json({ error : error.message }); //returning a formatted error if required   
+    }
 });
 
 
 //setting the port to which the server listens to for requests
-app.listen(PORT, 
-    () => { console.log(`listen to port ${PORT}`) }
-)
 
+initOracleConnection().then( () => {
+    app.listen(PORT, () => { 
+        console.log(`listen to port ${PORT}`);
+        // initOracleConnection();   //starting up the connection between backend and oracleDB
+    });
+});
 
 
 ///CRUD FUNCTIONS FOR THE SQL 
