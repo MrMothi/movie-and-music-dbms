86c0a10327cd41d846c6d77d608e9c75af31911c
MrMothi <mailtime2@gmail.com>
2024-11-17 17:23:25 -0500

    pushing everything to fix git issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,37 +9,8 @@ import {
 
 function App() {
 
-  //testing backend frontend connection with temp function to grab product table
-  const [data, setData] = useState('');
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try{
-        const response = await fetch('/get/product')
-        if (!response.ok) { 
-          throw new Error('Network response was not ok'); 
-        } 
-        const result = await response.json();
-        setData(result);   //if valid response then set the use state
-        console.log(data);
-      }
-      catch (error) {
-        console.log(error);
-      }
-      finally {
-        console.log("successfully fetched the backend text")
-      }
-    };
-
-    fetchData();
-
-  }, []);
-
-
-
   return (
     <div>
-      <p>{data.message}abc</p>
     </div>
   );
 }
