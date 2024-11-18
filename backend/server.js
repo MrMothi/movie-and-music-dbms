73c93adf39ae34b5f373cbda43c16c5d789d3695
MrMothi <mailtime2@gmail.com>
2024-11-17 20:23:35 -0500

    created working create functionality for the customer table, tested with postman

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const app = express();   //creating an express application
 require('dotenv').config(); //importing the env library
 const PORT = process.env.BACKEND_PORT;  //setting the server port to the environment variable
 
-//MIDDLEWARE
+//MIDDLEWARE=============================================================================
 app.use(cors()); // enabling cors
 app.use(express.json()); // getting express json reader functionality
 
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
 });
 
 
-
+//ORACLE CONNECTION AND DISCONNECT=============================================================================
 //not implementing the process pool strategy for database connections since there will only be 1 server instance running for a single frontend
 async function initOracleConnection() { 
      
@@ -75,7 +75,12 @@ app.get('/', (req, res) => {
 })
 
 
-app.get('/get/product', async (req, res) => { 
+//FETCH COMMANDS=============================================================================
+
+//READ/GET------------------------------------------------------------------------------------
+//7 functions, 1 for each table a generic select all/*
+//get product table
+app.get('/get/customer', async (req, res) => { 
     try{
         console.log(req.oracleConnection)
         const result = await req.oracleConnection.execute('SELECT * FROM customer'); 
@@ -90,8 +95,45 @@ app.get('/get/product', async (req, res) => {
 });
 
 
-//setting the port to which the server listens to for requests
 
+//POST/CREATE------------------------------------------------------------------------------------
+//7 Functions 1 for each table, which links to a form submit on the frontend
+//Create new customer
+app.post('/create/customer', async (req, res) => { 
+    try {
+        //destructuring the input req params
+        const { custid, firstname, lastname, email, phonenumber, address } = req.body;
+        // running the sql command to add a entry to the table and immediately commit it
+        const result = await req.oracleConnection.execute(
+            'INSERT INTO customer (customer_id, first_name, last_name, email, phone_number, address) VALUES (:custid, :firstname, :lastname, :email, :phonenumber, :address)',
+            [custid, firstname, lastname, email, phonenumber, address],
+            { autocommit: true } 
+        ); 
+        await req.oracleConnection.commit();
+
+        console.log("New row created in Customer Table with ID: ", custid);
+        res.status(201).json({message : "Customer created", result});    //sending back success message
+    } 
+    catch (error) {
+        res.status(500).json({ error : error.message }); //returning a formatted error if required   
+    }
+});
+
+
+// Expected object for above
+//  {
+//     "custid": 21,
+//     "firstname": "first1",
+//     "lastname": "last1",
+//     "email": "email1",
+//     "phonenumber": "phone1",
+//     "address": "address"
+//   }
+
+
+
+//BACKEND LISTENER=============================================================================
+//setting the port to which the server listens to for requests
 initOracleConnection().then( () => {
     app.listen(PORT, () => { 
         console.log(`listen to port ${PORT}`);
@@ -100,47 +142,48 @@ initOracleConnection().then( () => {
 });
 
 
-///CRUD FUNCTIONS FOR THE SQL 
-
-//CONNECT BACKEND WITH FRONT END
-
-//MAKE SIMPLE FRONT END..
 
 
 
-async function run() { 
-    let connection; 
-    try { 
-        connection = await oracledb.getConnection({ 
-            user: process.env.DB_USER, 
-            password: process.env.DB_PASS, 
-            connectString: process.env.DB_CONNECT_STRING
-        }); console.log('Successfully connected to Oracle Database'); 
-        // Execute a simple query 
-        const result = await connection.execute('SELECT * FROM customer'); 
-        console.log(result.rows); 
-    } 
-    catch (err) 
-    { 
-        console.error(err); 
-    } 
-    finally 
-    { 
-        if (connection) { 
-            try { 
-                await connection.close(); 
-            } 
-            catch (err) 
-            { 
-                console.error(err); 
-            } 
-        } 
-    } 
-} 
 
 
 
-run();
+/*
+BEYOND THIS POINT IS OLD CODE OR NOTES WE DONT NEED=============================================================================
+*/
+
+
+// ///CRUD FUNCTIONS FOR THE SQL
+// async function run() { 
+//     let connection; 
+//     try { 
+//         connection = await oracledb.getConnection({ 
+//             user: process.env.DB_USER, 
+//             password: process.env.DB_PASS, 
+//             connectString: process.env.DB_CONNECT_STRING
+//         }); console.log('Successfully connected to Oracle Database'); 
+//         // Execute a simple query 
+//         const result = await connection.execute('SELECT * FROM customer'); 
+//         console.log(result.rows); 
+//     } 
+//     catch (err) 
+//     { 
+//         console.error(err); 
+//     } 
+//     finally 
+//     { 
+//         if (connection) { 
+//             try { 
+//                 await connection.close(); 
+//             } 
+//             catch (err) 
+//             { 
+//                 console.error(err); 
+//             } 
+//         } 
+//     } 
+// } 
+// run();
 
 
 
